dad1d17c770fb18730b8566b8a6044fbfce1973f
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-08-23 09:28:08 -0500

    Create install-arch.sh

---
+++ install-arch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#desktop
+sudo pacman -Syy --needed labwc swaybg nwg-bar
+#screenshotting
+sudo pacman -Syy --needed grim slurp
+#device control
+sudo pacman -Syy --needed playerctl brightnessctl
+yay -S --needed wlrctl
+#sound
+sudo pacman -Syy --needed wpctl pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber
+#wallpaper script
+sudo pacman -Syy --needed python3-venv python3-pip pyenv
+#idle and lock
+sudo pacman -Syy --needed swayidle gtklock
