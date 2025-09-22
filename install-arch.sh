8d3f5fd532fc5c2a74ed30b070955d59a478f6fe
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-09-21 21:52:39 -0500

    Update install-arch.sh

--- install-arch.sh
+++ install-arch.sh
@@ -7,15 +7,14 @@ cd ~/yay && makepkg -si
 rm -rf ~/yay
 
 #desktop
-sudo pacman -Sy --needed --noconfirm labwc swaybg nwg-bar
+sudo pacman -Sy --needed --noconfirm labwc swaybg waybar pcmanfm-qt
 #screenshotting
 sudo pacman -Sy --needed --noconfirm grim slurp
 #device control
 sudo pacman -Sy --needed --noconfirm playerctl brightnessctl
-yay -S --needed --noconfirm wlrctl
 #sound
 sudo pacman -Sy --needed --noconfirm wireplumber pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber
-#wallpaper script
-sudo pacman -Sy --needed --noconfirm python3-venv python3-pip pyenv
 #idle and lock
-sudo pacman -Sy --needed --noconfirm swayidle gtklock
+sudo pacman -Sy --needed --noconfirm swayidle gtklock gtklock-powerbar-module
+#XDG
+sudo pacman -Sy --needed --noconfirm xdg-utils xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr
