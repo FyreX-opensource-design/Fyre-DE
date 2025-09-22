cb0225d2950307a8524d229addcf15a7979664b8
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-09-21 21:58:36 -0500

    Update install-common.sh

--- install-common.sh
+++ install-common.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 cp ./config ~/.config
-
-cd ~/ && git clone https://github.com/Nmoleo64/gtklock-powerbar-module
-cd ~/gtklock-powerbar-module && ninja -C build
-cd ~/gtklock-powerbar-module && sudo cp ./build/powerbar-module.so /usr/lib/gtklock
-rm -rf ~/gtklock-powerbar-module
+mkdir ~/.scripts
+cp ./scripts ~/.scripts
 #setup wallpaper
 cp ./wallpaper ~/.wallpaper
-cd ~/.wallpaper && pyenv install 3.11.7 && pyenv shell 3.11.7 && \ 
-python3 -m venv .wallpaper && \ 
-./.wallpaper/bin/pip3 install -r requirements.txt && \
-pyenv shell --unset
+python3 -m venv .wallpaper && ./.wallpaper/bin/pip3 install -r requirements.txt
