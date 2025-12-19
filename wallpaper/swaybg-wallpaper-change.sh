4e5875b1953f2211afd834e106c528b3145c54dd
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-12-19 10:24:26 -0600

    Delete wallpaper directory

--- wallpaper/swaybg-wallpaper-change.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -f /usr/share/bing-wallpaper/wallpaper.png ]; then
-    rm /usr/share/bing-wallpaper/wallpaper.png
-fi
-source .venv/bin/activate && python custom_bingwallpaper.py /usr/share/bing-wallpaper/wallpaper.png
-
-swaybg -i /usr/share/bing-wallpaper/wallpaper.png -m fill
-labrc -r
