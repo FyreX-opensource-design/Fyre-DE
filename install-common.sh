e47a8e57278bf5ccad66c99d6b83787e4e91d3c3
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-12-19 10:34:36 -0600

    Update install-common.sh

--- install-common.sh
+++ install-common.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 cp ./config ~/.config
-mkdir ~/.scripts
-cp ./scripts ~/.scripts
-#setup wallpaper
-cp ./wallpaper ~/.wallpaper
-python3 -m venv .wallpaper && ./.wallpaper/bin/pip3 install -r requirements.txt
